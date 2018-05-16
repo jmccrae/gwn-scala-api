0c510172d10912b66f6784b2a1404a3cf3d813cc
John P. McCrae <john@mccr.ae>
2018-05-16 17:00:06 +0100

    Continue work on building demo

--- web/src/main/scala/org/globalwordnet/api/web/GWNConverter.scala
+++ web/src/main/scala/org/globalwordnet/api/web/GWNConverter.scala
@@ -1,8 +1,32 @@
 package org.globalwordnet.api.web
 
 import org.scalatra._
+import org.scalatra.servlet.{FileUploadSupport, MultipartConfig, SizeConstraintExceededException, FileItem}
+import org.globalwordnet.api.serialize._
+import scala.util.Try
+import java.io.Reader
 
-class GWNConverter extends ScalatraServlet {
+class GWNConverter extends ScalatraServlet with FileUploadSupport {
+  configureMultipartHandling(MultipartConfig(maxFileSize = Some(3*1024*1024)))
+  
+  def processFile(item : FileItem) : Reader = {
+    new java.io.InputStreamReader(item.getInputStream)
+  }
+
+  post("/validate") {
+    val input = params("inputText") match {
+      case "" => processFile(fileParams("inputFile"))
+      case t => new java.io.StringReader(t)
+    }
+    params("inputFormat") match {
+      case "lmf" =>
+        Try(WNLMF.read(input))
+      case "json" =>
+        Try(WNJSON.read(input))
+      case "rdf" =>
+        Try(WNRDF.read(input, params("inputRdfLang"), ""))
+    }
+  }
 
   get("/") {
     views.html.index()
---
+++ web/src/main/webapp/gwn-converter.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+    "el": "#validator",
+    "data": {
+        "inputFormat": "lmf",
+        "inputRdfLang": "RDF/XML",
+        "inputText": "",
+    }
+});
