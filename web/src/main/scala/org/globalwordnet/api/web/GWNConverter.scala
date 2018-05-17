b90bfb94ee5f9dd0fac46875c1dee88837986843
John P. McCrae <john@mccr.ae>
2018-05-17 10:48:38 +0100

    Work on validation... now mostly works

--- web/src/main/scala/org/globalwordnet/api/web/GWNConverter.scala
+++ web/src/main/scala/org/globalwordnet/api/web/GWNConverter.scala
@@ -5,27 +5,51 @@ import org.scalatra.servlet.{FileUploadSupport, MultipartConfig, SizeConstraintE
 import org.globalwordnet.api.serialize._
 import scala.util.Try
 import java.io.Reader
+import javax.xml.parsers.DocumentBuilderFactory
+import org.xml.sax.{InputSource,ErrorHandler,SAXParseException}
 
 class GWNConverter extends ScalatraServlet with FileUploadSupport {
   configureMultipartHandling(MultipartConfig(maxFileSize = Some(3*1024*1024)))
   
+  def validateInitialXML(input : Reader) {
+    val factory = DocumentBuilderFactory.newInstance();
+    factory.setValidating(true)
+    val builder = factory.newDocumentBuilder();
+    builder.setErrorHandler(new ErrorHandler() {
+      override def error(exception : SAXParseException) { throw exception }
+      override def fatalError(exception : SAXParseException) { throw exception }
+      override def warning(exception : SAXParseException) { throw exception }
+    })
+    val document = builder.parse(new InputSource(input))
+    if(document.getDoctype() == null || 
+      "http://globalwordnet.github.io/schemas/WN-LMF-1.0.dtd" !=
+      document.getDoctype().getSystemId()) {
+        throw new XMLHeaderException("The DTD of the file must be http://globalwordnet.github.io/schemas/WN-LMF-1.0.dtd")
+    }
+  }
+
+
   def processFile(item : FileItem) : Reader = {
     new java.io.InputStreamReader(item.getInputStream)
   }
 
   post("/validate") {
-    val input = params("inputText") match {
+    def input = params("inputText") match {
       case "" => processFile(fileParams("inputFile"))
       case t => new java.io.StringReader(t)
     }
-    params("inputFormat") match {
+    val result = params("inputFormat") match {
       case "lmf" =>
-        Try(WNLMF.read(input))
+        Try(
+          validateInitialXML(input)
+        ).flatMap(_ => Try(WNLMF.read(input)))
       case "json" =>
         Try(WNJSON.read(input))
       case "rdf" =>
         Try(WNRDF.read(input, params("inputRdfLang"), ""))
     }
+    println(result)
+    views.html.validate(result)
   }
 
   get("/") {
@@ -33,3 +57,5 @@ class GWNConverter extends ScalatraServlet with FileUploadSupport {
   }
 
 }
+
+class XMLHeaderException(msg : String) extends RuntimeException(msg)
